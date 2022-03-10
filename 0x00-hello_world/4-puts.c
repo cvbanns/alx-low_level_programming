e03d66b430760d624e937acf3f2c51e6e578161d
cvbanns <calebaregbesola@gmail.com>
2022-03-10 06:32:24 -0800

    My first c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*
+ * main - program startup
+ * Description: prints the exact phrase followed by a new line
+ * Return: return (0) is the required function signature
+ */
+
+#include <stdio.h>
+int main(void)
+{
+		puts("Programming is like building a multilingual puzzle\n");
+			return (0);
+}
