7e8118497163388be4c3b313b5aed8e2f56c5de1
cvbanns <calebaregbesola@gmail.com>
2022-03-10 06:35:52 -0800

    My first c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,26 @@
-/*
- * main - program startup
- * Description: prints the exact phrase followed by a new line
- * Return: return (0) is the required function signature
- */
+/**
+ *
+ *  * main - program startup
+ *
+ *  (*
+ *
+ *   * Description: prints the exact phrase followed by a new line)?
+ *
+ *    * Return: return (0) is the required function signature
+ *
+ *     */
+
+
+
 
 #include <stdio.h>
+
 int main(void)
+
 {
-		puts("Programming is like building a multilingual puzzle\n");
+
+		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
+
 }
