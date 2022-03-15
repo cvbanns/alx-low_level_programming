15a88f0bcb4f64f954a0e53a4ca4769fc7e5a303
cvbanns <calebaregbesola@gmail.com>
2022-03-15 08:14:04 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: computes and prints the sum of all the multiples of 3 or
- * 5 below 1024 (excluded), followed by a new line
- * Return: 0
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ * 1 and 2, separated by a comma followed by a space.
+ * Return: Always 0.
  */
 int main(void)
 {
-int i = 0;
-unsigned long int a = 0, b = 1, next = 0;
-while (i < 98)
+int count;
+unsigned long fb1 = 0, fb2 = 1, sum;
+unsigned long fb1_half1, fb1_half2, fb2_half1, fb2_half2;
+unsigned long half1, half2;
+for (count = 0; count < 92; count++)
 {
-next = a + b;
-a = b;
-b = next;
-printf("%lu", next);
-if (i < 97)
-printf(", ");
-i++;
+sum = fb1 + fb2;
+printf("%lu, ", sum);
+fb1 = fb2;
+fb2 = sum;
 }
-putchar('\n');
+fb1_half1 = fb1 / 10000000000;
+fb2_half1 = fb2 / 10000000000;
+fb1_half2 = fb1 % 10000000000;
+fb2_half2 = fb2 % 10000000000;
+for (count = 93; count < 99; count++)
+{
+half1 = fb1_half1 + fb2_half1;
+half2 = fb1_half2 + fb2_half2;
+if (fb1_half2 + fb2_half2 > 9999999999)
+{
+half1 += 1;
+half2 %= 10000000000;
+}
+printf("%lu%lu", half1, half2);
+if (count != 98)
+printf(", ");
+fb1_half1 = fb2_half1;
+fb1_half2 = fb2_half2;
+fb2_half1 = half1;
+fb2_half2 = half2;
+}
+printf("\n");
 return (0);
 }
