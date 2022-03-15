07f605c36cf9f65942970f80ab52066428f1ef12
cvbanns <calebaregbesola@gmail.com>
2022-03-15 00:02:19 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs  - Short description, single line
+ * @n: contians value
+ * Return: returns absolute value of n
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+n = (-1) * n;
+}
+return (n);
+}
