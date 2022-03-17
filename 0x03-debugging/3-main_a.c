293269d0ad1779f4036925bd377ab6b66e48f673
cvbanns <calebaregbesola@gmail.com>
2022-03-16 17:27:13 -0700

    task-3

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -12,11 +12,16 @@ int main(void)
 int month;
 int day;
 int year;
-month = 12;
-day = 31;
-year = 2022;
+
+month = 4;
+day = 01;
+year = 1997;
+
 printf("Date: %02d/%02d/%04d\n", month, day, year);
+
 day = convert_day(month, day);
+
 print_remaining_days(month, day, year);
+
 return (0);
 }
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,19 +2,7 @@
 #include "main.h"
 
 /**
- * print_days - prints days
- * @day: day
- * @days: days in the year
- *
- * Return: void
- */
-void print_days(int day, int days)
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", days - day);
-}
- /**
- * print_remaining_days - takes a date and printls how many days are
+ * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
  * @month: month in number format
  * @day: day of month
@@ -24,27 +12,15 @@ printf("Remaining days: %d\n", days - day);
 
 void print_remaining_days(int month, int day, int year)
 {
-if (year % 4 == 0)
+if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
-if (year % 400 == 0)
+if (month > 2 && day >= 60)
 {
-if (month >= 2 && day >= 60)
 day++;
-print_days(day, 366);
 }
-else if (year % 100 == 0)
-{
-if (month == 2 && day == 60)
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-else
-print_days(day, 365);
-}
-else
-{
-if (month >= 2 && day >= 60)
-day++;
-print_days(day, 366);
-}
+
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
@@ -53,6 +29,9 @@ if (month == 2 && day == 60)
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
-print_days(day, 365);
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
+}
 }
 }
