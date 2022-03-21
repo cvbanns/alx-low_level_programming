13203dc251c4184ffd8f4011ddf782f2763f6a9d
cvbanns <calebaregbesola@gmail.com>
2022-03-21 02:41:20 -0700

    dont swap horses swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a: declaration of a and paramters for the function swap_int
+ * @b: declaration of b and paramters for the function swap_int
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+}
