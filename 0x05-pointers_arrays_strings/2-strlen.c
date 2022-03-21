855115bb1637d58fa8895885230be0d12eec74ae
cvbanns <calebaregbesola@gmail.com>
2022-03-21 02:59:47 -0700

    what is the lenght of this string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+int c;
+
+for (c = 0; s[c] != 0; c++)
+{
+}
+return (c);
+}
