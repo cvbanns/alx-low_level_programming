dd7661f042e400ab8756179125854e7633fdf04e
cvbanns <calebaregbesola@gmail.com>
2022-03-29 22:39:11 -0700

    Recursion task 101 advanced correction

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,52 +1,74 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * last_index - returns the last index of a string (counts the null char)
- * @s: pointer the string
- * Return: int
+ * wildcmp - check the code for Holberton School students.
+ * @s1: string
+ * @s2: string
+ * @a: int
+ * @b: int
+ * @wildUsed: int
+ * Return: Always 0.
  */
-int is_palindrome(char *s);
-int check(char *s, int start, int end, int pair);
-int last_index(char *s)
+int checker(char *s1, char *s2, int a, int b, int wildUsed);
+int wildcmp(char *s1, char *s2)
 {
-int n = 0;
-
-if (*s > '\0')
-n += last_index(s + 1) + 1;
-
-return (n);
+return (checker(s1, s2, 0, 0, -1));
 }
 
 /**
- * is_palindrome - check if a string is a palindrome
- * @s: string to check
- * Return: 0 or 1
- */
-
-int is_palindrome(char *s)
-{
-int end = last_index(s);
-
-return (check(s, 0, end - 1, end % 2));
-}
-
-/**
- * check - checker for the palindrome
+ * checkLast - check last char of s2 when s1 ends
  * @s: string
- * @start: int moves from right to left
- * @end: int moves from left to right
- * @pair: int
+ * @i: int
  * Return: 0 or 1
  */
+int checkLast(char *s, int i)
+{
+if (s[i] == '*')
+return (checkLast(s, i + 1));
+else if (s[i] == '\0')
+return (1);
 
+return (0);
 
-int check(char *s, int start, int end, int pair)
+}
+/**
+ * checker - helper
+ * @s1: string
+ * @s2: string
+ * @a: int
+ * @b: int
+ * @wildUsed: int*Return: Always 0.
+ */
+int checker(char *s1, char *s2, int a, int b, int wildUsed)
 {
 
-if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
-return (1);
-else if (s[start] != s[end])
+if (s1[a] != '\0')
+{
+if (s2[b] == '\0')
 return (0);
-else
-return (check(s, start + 1, end - 1, pair));
+else if (s2[b] == '*')
+{
+if (s2[b + 1] == '*')
+return (checker(s1, s2, a, b + 1, b));
+else if (s2[b + 1] == s1[a])
+return (checker(s1, s2, a, b + 1, b));
+else if (s1[a + 1] != s2[b + 1])
+return (checker(s1, s2, a + 1, b, b));
+else if (s1[a + 1] == s2[b + 1])
+return (checker(s1, s2, a + 1, b + 1, b));
+}
+else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
+return (checker(s1, s2, a + 1, b + 1, wildUsed));
+
+if (wildUsed == -1)
+return (0);
+return (checker(s1, s2, a, wildUsed, wildUsed));
+
+}
+if (s2[b] != '\0')
+return (checkLast(s2, b));
+
+return (1);
+
 }
