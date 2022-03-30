2d0130d504f88cb39c2a80ed5acaa07a413c9444
cvbanns <calebaregbesola@gmail.com>
2022-03-29 22:42:21 -0700

    Yet another correction for 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -28,9 +28,7 @@ if (s[i] == '*')
 return (checkLast(s, i + 1));
 else if (s[i] == '\0')
 return (1);
-
 return (0);
-
 }
 /**
  * checker - helper
@@ -42,7 +40,6 @@ return (0);
  */
 int checker(char *s1, char *s2, int a, int b, int wildUsed)
 {
-
 if (s1[a] != '\0')
 {
 if (s2[b] == '\0')
@@ -60,15 +57,11 @@ return (checker(s1, s2, a + 1, b + 1, b));
 }
 else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
 return (checker(s1, s2, a + 1, b + 1, wildUsed));
-
 if (wildUsed == -1)
 return (0);
 return (checker(s1, s2, a, wildUsed, wildUsed));
-
 }
 if (s2[b] != '\0')
 return (checkLast(s2, b));
-
 return (1);
-
 }
