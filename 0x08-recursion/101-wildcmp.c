1224a35ad48df5cdec44941534371ead9f307806
cvbanns <calebaregbesola@gmail.com>
2022-03-30 14:35:33 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,9 +5,12 @@
  * wildcmp - check the code for Holberton School students.
  * @s1: string
  * @s2: string
+ * @a: int
+ * @b: int
+ * @wildUsed: int
  * Return: Always 0.
  */
-
+int checker(char *s1, char *s2, int a, int b, int wildUsed);
 int wildcmp(char *s1, char *s2)
 {
 return (checker(s1, s2, 0, 0, -1));
@@ -58,8 +61,8 @@ return (checker(s1, s2, a + 1, b + 1, b));
 }
 else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
 return (checker(s1, s2, a + 1, b + 1, wildUsed));
-if (wildUsed == -1)
 
+if (wildUsed == -1)
 return (0);
 
 return (checker(s1, s2, a, wildUsed, wildUsed));
